a2336fb43a62c9030dc24fd165d23ff04a606e75
TomSssM <ilyashome3@gmail.com>
2022-01-23 05:11:27 +0300

    make isPresent.js more useful

--- isPresent.js
+++ isPresent.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const { run, exitWithError, blue, getSuccessMessage } = require('./utils');
 const { readChecksumDbToMap, serializeChecksumMapDb } = require('./utils/checksum');
 const { diff } = require('./utils/map');
@@ -6,6 +7,7 @@ const argv = process.argv.slice(2);
 
 const fromArg = argv.indexOf('--from'); // Are checksums from --from ...
 const inArg = argv.indexOf('--in');     // ... present in --in ?
+const outArg = argv.indexOf('--out');
 
 if (fromArg === -1 || inArg === -1) {
   exitWithError(
@@ -17,6 +19,7 @@ if (fromArg === -1 || inArg === -1) {
 
 const fromValue = argv[fromArg + 1];
 const inValue = argv[inArg + 1];
+const outValue = outArg !== -1 && argv[outArg + 1];
 
 /**
  * similar to cmpFilesDb @see cmpFilesDb.js
@@ -24,15 +27,33 @@ const inValue = argv[inArg + 1];
 run(async () => {
     const inDb = await readChecksumDbToMap(inValue);
     const fromDb = await readChecksumDbToMap(fromValue);
-    const missingChecksums = serializeChecksumMapDb(diff(fromDb, inDb));
+    const [diffMap, correspondsMap] = diff(fromDb, inDb).map((db) => serializeChecksumMapDb(db) || '...empty');
 
-    if (missingChecksums) {
-      throw new Error(`
-        missing checksums:
+    const msg = `
 
-        ${missingChecksums}
-      `);
+DIFF:
+${diffMap}
+
+CORRESPONDS:
+${correspondsMap}
+
+`;
+
+    if (outValue) {
+      fs.writeFileSync(outValue, msg.trim() + '\n', { encoding: 'utf8' });
+      console.log(`${blue('all data written to:')} ${outValue}`);
+
+      return console.log(getSuccessMessage());
     }
 
+    // if (missingChecksums) {
+    //   throw new Error(`
+    //     missing checksums:
+
+    //     ${missingChecksums}
+    //   `);
+    // }
+
+    console.log(msg);
     console.log(getSuccessMessage());
 });
--- utils/map.js
+++ utils/map.js
@@ -1,4 +1,4 @@
-const { diff: setDiff } = require('./set');
+// const { diff: setDiff } = require('./set');
 
 /**
  * @param {Map<string, string[]>} map
@@ -18,10 +18,20 @@ function addToMap(map) {
  * return values of map1 missing from map2
  * @param {Map<string, string[]>} map1
  * @param {Map<string, string[]>} map2
- * @returns {Map<string, string[]>}
+ * @returns {[diffMap: Map<string, string[]>, correspondsMap: Map<string, string[]>]}
  */
 function diff(map1, map2) {
   const diffMap = new Map();
+  const correspondsMap = new Map();
+
+  const setCorresponds = (file1, file2) => {
+    if (file1 === file2) return;
+
+    const k = `${file1} =>`;
+    const v = correspondsMap.get(k) ?? [];
+    v.push(file2);
+    correspondsMap.set(k, v);
+  };
 
   for (const [k, v1] of map1.entries()) {
     const v2 = map2.get(k);
@@ -29,18 +39,48 @@ function diff(map1, map2) {
     if (!v2 && v1.length) {
       diffMap.set(k, v1);
     } else if (v1.length && v2.length) {
-      const valuesDiff = Array.from(setDiff(
-        new Set(v1),
-        new Set(v2)
-      ));
 
-      if (valuesDiff.length) {
-        diffMap.set(k, valuesDiff);
+      if (
+        v1.length !== v2.length ||
+        v1.some((file) => !v2.includes(file)) ||
+        v2.some((file) => !v1.includes(file))
+      ) {} else continue;
+
+      const v1Copy = [...v1];
+      const v2Copy = [...v2];
+
+      let file1;
+      let file2;
+
+      while (v1Copy.length) {
+        file1 = v1Copy.shift();
+        file2 = v2Copy.shift();
+
+        if (file2) {
+          setCorresponds(file1, file2);
+        } else {
+          setCorresponds(file1, '<COPIED>');
+        }
       }
+
+      if (v2Copy.length) {
+        v2Copy.forEach((file2) => {
+          setCorresponds('<MISSING>', file2);
+        });
+      }
+
+      // const valuesDiff = Array.from(setDiff(
+      //   new Set(v1),
+      //   new Set(v2)
+      // ));
+
+      // if (valuesDiff.length) {
+      //   diffMap.set(k, valuesDiff);
+      // }
     }
   }
 
-  return diffMap;
+  return [diffMap, correspondsMap];
 }
 
 /**

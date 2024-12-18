95687df93fa2788ca84f73a16311c978ca9af086
TomSssM <ilyashome3@gmail.com>
2024-12-19 02:28:05 +0300

    decompose buffer-3-0

--- fs-n-checksums-sync.sh
+++ fs-n-checksums-sync.sh
@@ -1,7 +1,8 @@
 mkdir ~/Projects/scripts/files && \
 cd /h/ && \
 find ./Buffer/2-0 -type f >> ~/Projects/scripts/files/buffer-2-0.txt && \
-find ./Buffer/3-0 -type f >> ~/Projects/scripts/files/buffer-3-0.txt && \
+find ./Buffer/3-0 -not -path './Buffer/3-0/Updates/*' -type f >> ~/Projects/scripts/files/buffer-3-0.txt && \
+find ./Buffer/3-0/Updates -type f >> ~/Projects/scripts/files/buffer-3-0-updates.txt && \
 cd ./Buffer && \
 find './Storage HD' -type f >> ~/Projects/scripts/files/storage-hd.txt && \
 find './Torrentino 4' -type f >> ~/Projects/scripts/files/torrentino-4.txt && \
@@ -15,6 +16,7 @@ cd ~/Projects/scripts && \
 echo && \
 echo -n '2-0:                               ' && cat ./files/buffer-2-0.txt | wc -l && \
 echo -n '3-0:                               ' && cat ./files/buffer-3-0.txt | wc -l && \
+echo -n '3-0 (Updates):                     ' && cat ./files/buffer-3-0-updates.txt | wc -l && \
 echo -n 'Storage HD:                        ' && cat ./files/storage-hd.txt | wc -l && \
 echo -n 'Torrentino 4:                      ' && cat ./files/torrentino-4.txt | wc -l && \
 echo -n 'Torrentino 5:                      ' && cat ./files/torrentino-5.txt | wc -l && \
@@ -26,6 +28,7 @@ echo -n 'Torrentino HD (widescreen rips):   ' && cat ./files/torrentino-hd-wide.
 echo && \
 echo -n '2-0:                               ' && node na-kolenke.js --files ./files/buffer-2-0.txt --checksum ../checksums-2/db/sha1/buffer-2-0.txt && \
 echo -n '3-0:                               ' && node na-kolenke.js --files ./files/buffer-3-0.txt --checksum ../checksums-2/db/sha1/buffer-3-0.txt && \
+echo -n '3-0 (Updates):                     ' && node na-kolenke.js --files ./files/buffer-3-0-updates.txt --checksum ../checksums-2/db/sha1/buffer-3-0-updates.txt && \
 echo -n 'Storage HD:                        ' && node na-kolenke.js --files ./files/storage-hd.txt --checksum ../checksums-2/db/sha1/storage-hd.txt && \
 echo -n 'Torrentino 4:                      ' && node na-kolenke.js --files ./files/torrentino-4.txt --checksum ../checksums-2/db/sha1/torrentino-4.txt && \
 echo -n 'Torrentino 5:                      ' && node na-kolenke.js --files ./files/torrentino-5.txt --checksum ../checksums-2/db/sha1/torrentino-5.txt && \

87f8f6e9123517fc825b1beefe15994135d837f4
TomSssM <ilyashome3@gmail.com>
2025-12-20 22:24:09 +0300

    update the fs-n-checksums-sync scripts

--- docs/fs-n-checksums-sync.md
+++ docs/fs-n-checksums-sync.md
@@ -2,15 +2,69 @@
 
 Check that the files on the file system match the Checksums Database:
 
-__The Holidays__
+---
+
+### The Holidays
 ```shell
 DRIVE='/h' bash ./fs-n-checksums-sync.sh
 ```
 
-__Buffer__
+#### Buffer
 ```shell
 DRIVE='/h' bash ./fs-n-checksums-sync-2.sh
 ```
+
+#### Buffer (The Holidays Base)
+
+```shell
+DRIVE='/h' bash ./fs-n-checksums-sync-base-2.sh
+```
+
+#### Buffer (The Holidays DVD)
+
+```shell
+DRIVE='/h' bash ./fs-n-checksums-sync-dvd-2.sh
+```
+
+#### Buffer (The Holidays HD)
+
+```shell
+DRIVE='/h' bash ./fs-n-checksums-sync-hd-2.sh
+```
+
+---
+
+### Выходные
+
+```shell
+DRIVE='/h' bash ./fs-n-checksums-sync-legacy-stuff.sh
+```
+
+---
+
+### The Holidays Base
+
+```shell
+DRIVE='/h' bash ./fs-n-checksums-sync-base.sh
+```
+
+---
+
+### The Holidays DVD
+
+```shell
+DRIVE='/h' bash ./fs-n-checksums-sync-dvd.sh
+```
+
+---
+
+### The Holidays HD
+
+```shell
+DRIVE='/h' bash ./fs-n-checksums-sync-hd.sh
+```
+
+---
 
 ## Specification
 
--- fs-n-checksums-sync.sh
+++ fs-n-checksums-sync.sh
@@ -4,29 +4,39 @@ if [ -d "${SCRIPT_DIR}/files" ]; then
   rm -r "${SCRIPT_DIR}/files"
 fi
 mkdir "${SCRIPT_DIR}/files" && \
+mkdir "${SCRIPT_DIR}/files/files" && \
+mkdir "${SCRIPT_DIR}/files/checksums" && \
 cd "${DRIVE}/The Holidays" && \
-find './Code' -type f >> "${SCRIPT_DIR}/files/code.txt" && \
-find './Data' -type f >> "${SCRIPT_DIR}/files/data.txt" && \
-find './Docs' -type f >> "${SCRIPT_DIR}/files/docs.txt" && \
-find './My Library' -type f >> "${SCRIPT_DIR}/files/my-library.txt" && \
-find './Pictures' -type f >> "${SCRIPT_DIR}/files/pictures.txt" && \
-find './The Storybook' -type f >> "${SCRIPT_DIR}/files/the-storybook.txt" && \
-find './TV' -type f >> "${SCRIPT_DIR}/files/tv.txt" && \
+find './Code' -type f >> "${SCRIPT_DIR}/files/files/code.txt" && \
+find './Data' -type f >> "${SCRIPT_DIR}/files/files/data.txt" && \
+find './Docs' -type f >> "${SCRIPT_DIR}/files/files/docs.txt" && \
+find './My Library' -type f >> "${SCRIPT_DIR}/files/files/my-library.txt" && \
+find './Pictures' -type f >> "${SCRIPT_DIR}/files/files/pictures.txt" && \
+find './The Storybook' -type f >> "${SCRIPT_DIR}/files/files/the-storybook.txt" && \
+find './TV' -type f >> "${SCRIPT_DIR}/files/files/tv.txt" && \
+cd "${CHECKSUMS_DIR}" && \
+find './db/sha1' -path './db/sha1/code.sha1' -exec cat {} \; >> "${SCRIPT_DIR}/files/checksums/code.sha1" && \
+find './db/sha1' -path './db/sha1/data.sha1' -exec cat {} \; >> "${SCRIPT_DIR}/files/checksums/data.sha1" && \
+find './db/sha1' -path './db/sha1/docs.sha1' -exec cat {} \; >> "${SCRIPT_DIR}/files/checksums/docs.sha1" && \
+find './db/sha1' -path './db/sha1/my-library.sha1' -exec cat {} \; >> "${SCRIPT_DIR}/files/checksums/my-library.sha1" && \
+find './db/sha1' -path './db/sha1/pictures.sha1' -exec cat {} \; >> "${SCRIPT_DIR}/files/checksums/pictures.sha1" && \
+find './db/sha1' -path './db/sha1/the-storybook.sha1' -exec cat {} \; >> "${SCRIPT_DIR}/files/checksums/the-storybook.sha1" && \
+find './db/sha1' -path './db/sha1/tv-*.sha1' -exec cat {} \; >> "${SCRIPT_DIR}/files/checksums/tv.sha1" && \
 cd "${SCRIPT_DIR}" && \
 echo && \
-echo -n 'Code:                               ' && cat "${SCRIPT_DIR}/files/code.txt" | wc -l && \
-echo -n 'Data:                               ' && cat "${SCRIPT_DIR}/files/data.txt" | wc -l && \
-echo -n 'Docs:                               ' && cat "${SCRIPT_DIR}/files/docs.txt" | wc -l && \
-echo -n 'My Library:                         ' && cat "${SCRIPT_DIR}/files/my-library.txt" | wc -l && \
-echo -n 'Pictures:                           ' && cat "${SCRIPT_DIR}/files/pictures.txt" | wc -l && \
-echo -n 'The Storybook:                      ' && cat "${SCRIPT_DIR}/files/the-storybook.txt" | wc -l && \
-echo -n 'TV:                                 ' && cat "${SCRIPT_DIR}/files/tv.txt" | wc -l && \
+echo -n 'Code:                               ' && cat "${SCRIPT_DIR}/files/files/code.txt" | wc -l && \
+echo -n 'Data:                               ' && cat "${SCRIPT_DIR}/files/files/data.txt" | wc -l && \
+echo -n 'Docs:                               ' && cat "${SCRIPT_DIR}/files/files/docs.txt" | wc -l && \
+echo -n 'My Library:                         ' && cat "${SCRIPT_DIR}/files/files/my-library.txt" | wc -l && \
+echo -n 'Pictures:                           ' && cat "${SCRIPT_DIR}/files/files/pictures.txt" | wc -l && \
+echo -n 'The Storybook:                      ' && cat "${SCRIPT_DIR}/files/files/the-storybook.txt" | wc -l && \
+echo -n 'TV:                                 ' && cat "${SCRIPT_DIR}/files/files/tv.txt" | wc -l && \
 echo && \
-echo -n 'Code:                               ' && node na-kolenke.js --files "${SCRIPT_DIR}/files/code.txt" --checksum "${CHECKSUMS_DIR}/db/sha1/code.sha1" && \
-echo -n 'Data:                               ' && node na-kolenke.js --files "${SCRIPT_DIR}/files/data.txt" --checksum "${CHECKSUMS_DIR}/db/sha1/data.sha1" && \
-echo -n 'Docs:                               ' && node na-kolenke.js --files "${SCRIPT_DIR}/files/docs.txt" --checksum "${CHECKSUMS_DIR}/db/sha1/docs.sha1" && \
-echo -n 'My Library:                         ' && node na-kolenke.js --files "${SCRIPT_DIR}/files/my-library.txt" --checksum "${CHECKSUMS_DIR}/db/sha1/my-library.sha1" && \
-echo -n 'Pictures:                           ' && node na-kolenke.js --files "${SCRIPT_DIR}/files/pictures.txt" --checksum "${CHECKSUMS_DIR}/db/sha1/pictures.sha1" && \
-echo -n 'The Storybook:                      ' && node na-kolenke.js --files "${SCRIPT_DIR}/files/the-storybook.txt" --checksum "${CHECKSUMS_DIR}/db/sha1/the-storybook.sha1" && \
-echo -n 'TV:                                 ' && node na-kolenke.js --files "${SCRIPT_DIR}/files/tv.txt" --checksum "${CHECKSUMS_DIR}/db/sha1/tv.sha1"
+echo -n 'Code:                               ' && node na-kolenke.js --files "${SCRIPT_DIR}/files/files/code.txt" --checksum "${SCRIPT_DIR}/files/checksums/code.sha1" && \
+echo -n 'Data:                               ' && node na-kolenke.js --files "${SCRIPT_DIR}/files/files/data.txt" --checksum "${SCRIPT_DIR}/files/checksums/data.sha1" && \
+echo -n 'Docs:                               ' && node na-kolenke.js --files "${SCRIPT_DIR}/files/files/docs.txt" --checksum "${SCRIPT_DIR}/files/checksums/docs.sha1" && \
+echo -n 'My Library:                         ' && node na-kolenke.js --files "${SCRIPT_DIR}/files/files/my-library.txt" --checksum "${SCRIPT_DIR}/files/checksums/my-library.sha1" && \
+echo -n 'Pictures:                           ' && node na-kolenke.js --files "${SCRIPT_DIR}/files/files/pictures.txt" --checksum "${SCRIPT_DIR}/files/checksums/pictures.sha1" && \
+echo -n 'The Storybook:                      ' && node na-kolenke.js --files "${SCRIPT_DIR}/files/files/the-storybook.txt" --checksum "${SCRIPT_DIR}/files/checksums/the-storybook.sha1" && \
+echo -n 'TV:                                 ' && node na-kolenke.js --files "${SCRIPT_DIR}/files/files/tv.txt" --checksum "${SCRIPT_DIR}/files/checksums/tv.sha1"
 rm -r "${SCRIPT_DIR}/files"
